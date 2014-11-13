8690b95bde4daabc3aacd79cd7ebeacfdb2dd369
Greg Keane <gkeane@gmail.com>
2014-11-13 18:19:12 -0500

    update attr

--- php-fpm/attributes/default.rb
+++ php-fpm/attributes/default.rb
@@ -1,24 +1,11 @@
-if node.platform_family == "rhel"
-  user = "nginx"
-  group = "nginx"
-  conf_dir = "/etc/php.d"
-  pool_conf_dir = "/etc/php-fpm.d"
-  conf_file = "/etc/php-fpm.conf"
-  error_log = "/var/log/php-fpm/error.log"
-  pid = "/var/run/php-fpm/php-fpm.pid"
-else
-  user = "www-data"
-  group = "www-data"
-  conf_dir = "/etc/php5/fpm/conf.d"
-  pool_conf_dir = "/etc/php5/fpm/pool.d"
-  if node.platform == "ubuntu" and node.platform_version.to_f <= 10.04
-    conf_file = "/etc/php5/fpm/php5-fpm.conf"
-  else
-    conf_file = "/etc/php5/fpm/php-fpm.conf"
-  end
-  error_log = "/var/log/php5-fpm.log"
-  pid ="/var/run/php5-fpm.pid"
-end
+user = "nginx"
+group = "nginx"
+conf_dir = "/etc/php.d"
+pool_conf_dir = "/etc/php-fpm.d"
+conf_file = "/etc/php-fpm.conf"
+error_log = "/var/log/php-fpm/error.log"
+pid = "/var/run/php-fpm/php-fpm.pid"
+
 
 default['php-fpm']['user'] = user
 default['php-fpm']['group'] = group
