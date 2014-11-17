84b3fe36897d56a077354817c35d0911fbcdaf18
Greg Keane <gkeane@gmail.com>
2014-11-17 12:09:25 -0500

    update install

--- php-fpm/recipes/install.rb
+++ php-fpm/recipes/install.rb
@@ -41,10 +41,7 @@ else
   php_fpm_service_name = node['php-fpm']['service_name']
 end
 
-service_provider = nil
-if node['platform'] == 'ubuntu' and node['platform_version'].to_f >= 13.10
-  service_provider = ::Chef::Provider::Service::Upstart
-end
+
 
 directory node['php-fpm']['log_dir']
 
