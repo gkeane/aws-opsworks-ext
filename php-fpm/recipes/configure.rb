9e6a4e59be21fb75175801808b45b6260fcc3f06
Greg Keane <gkeane@gmail.com>
2014-11-17 11:58:46 -0500

    update config

--- php-fpm/recipes/configure.rb
+++ php-fpm/recipes/configure.rb
@@ -45,3 +45,10 @@ if node['php-fpm']['pools']
     end
   end
 end
+
+service "php-fpm" do
+  provider service_provider if service_provider
+  service_name php_fpm_service_name
+  supports :start => true, :stop => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
