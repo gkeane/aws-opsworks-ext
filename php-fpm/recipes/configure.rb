cd8765fb45e894c558112fc09e9a10a1beec4946
Greg Keane <gkeane@gmail.com>
2014-11-17 12:06:52 -0500

    update config

--- php-fpm/recipes/configure.rb
+++ php-fpm/recipes/configure.rb
@@ -46,6 +46,11 @@ if node['php-fpm']['pools']
   end
 end
 
+service_provider = nil
+if node['platform'] == 'ubuntu' and node['platform_version'].to_f >= 13.10
+  service_provider = ::Chef::Provider::Service::Upstart
+end
+
 service "php-fpm" do
   provider service_provider if service_provider
   service_name php_fpm_service_name
