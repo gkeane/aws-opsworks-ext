94440098b71fcc904c257e610b871a7b621e4689
Greg Keane <gkeane@gmail.com>
2014-11-20 13:16:21 -0500

    deploy

---
+++ php-fpm/recipes/deploy.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: php-fpm
+# Recipe:: deploy
+#
+ 
+include_recipe 'deploy'
+include_recipe 'nginx::service'
+ 
+node[:deploy].each do |application, deploy|
+  # We're not using rails apps. This this will never be overridden in a php-fpm deploy as custom json. 
+  # Ddd this to deploy, so that way we can make sure only this app is deployed.
+  unless defined?(deploy[:rails_env])
+    Chef::Log.debug("Skipping php-fpm::deploy application #{application} as it is not the application being deployed")
+    next
+  else
+    Chef::Log.debug("Deploying application #{application}.")
+  end
+ 
+  if deploy[:application_type] != 'php' 
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP-FPM app")
+    next
+  end
+ 
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+ 
+  opsworks_deploy do
+    app application
+    deploy_data deploy
+  end
+ 
+  nginx_web_app application do
+    application deploy
+    cookbook deploy.has_key?("application_alias") ? deploy[:application_alias] : application
+  end
+ 
+end
