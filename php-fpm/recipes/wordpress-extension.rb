2a9087aae314ef271a1e623ae215d597d145105e
Greg Keane <gkeane@gmail.com>
2015-06-16 12:57:02 -0400

    add ext

---
+++ php-fpm/recipes/wordpress-extension.rb
@@ -0,0 +1,7 @@
+action :create do
+  template "#{node['nginx']['dir']}/wordpress.conf" do
+    source   'wordpress-extension.erb'
+    owner    'root'
+    group    'root'
+    mode     00644
+  end
