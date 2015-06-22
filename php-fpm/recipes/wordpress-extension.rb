8e4a29093bdf0cbfe089d9ebd2578f4789345909
Greg Keane <gkeane@gmail.com>
2015-06-22 14:00:22 -0400

    wpext

--- php-fpm/recipes/wordpress-extension.rb
+++ php-fpm/recipes/wordpress-extension.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|  
-  template "#{node['nginx']['dir']}/wordpress.conf" do
+  template "#{node['nginx']['dir']}/conf.d/wordpress.conf" do
     source   'wordpress-extension.erb'
     owner    'root'
     group    'root'
