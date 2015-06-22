da826892891255a256345a276f3371ab09808db0
Greg Keane <gkeane@gmail.com>
2015-06-22 14:38:36 -0400

    uploads default

--- php-fpm/attributes/default.rb
+++ php-fpm/attributes/default.rb
@@ -6,6 +6,7 @@ conf_file = "/etc/php-fpm.conf"
 error_log = "/var/log/php-fpm/error.log"
 pid = "/var/run/php-fpm/php-fpm.pid"
 
+default['wordpress_nginx']['s3_forward'] = 'https://s3.amazonaws.com/udextension/wp-content/uploads/'
 default['wordpress_nginx']['fastcgi_read_timeout'] = '3600s'
 default['wordpress_nginx']['static_file_expiry'] = '24h'
 
