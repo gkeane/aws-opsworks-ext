b94cce26c54cd96073d97e050a0d8ce55cb7dcf1
Greg Keane <gkeane@gmail.com>
2015-07-09 14:47:14 -0400

    new erb

--- php-fpm/recipes/deploy.rb
+++ php-fpm/recipes/deploy.rb
@@ -35,6 +35,18 @@ node[:deploy].each do |application, deploy|
  
   nginx_web_app application do
     application deploy
+    template "#{node['nginx']['dir']}/global/wordpress.conf" do
+      source   'wordpress.erb'
+      owner    'root'
+      group    'root'
+      mode     00644
+    end
+    template "#{node['nginx']['dir']}/global/restrictions.conf" do
+      source   'restrictions.erb'
+      owner    'root'
+      group    'root'
+      mode     00644
+    end
   #  cookbook deploy.has_key?("application_alias") ? deploy[:application_alias] : application
   end
  
